6cdfd3408cf6f51bf1090308fa0accda8dd71fa5
hyunsoo kim <ghgfhfg2@naver.com>
2021-04-16 17:35:51 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,113 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
+import './custom_antd.less';
+import "./App.css";
+import Join from "./component/Join";
+import Login from "./component/Login";
+import Loading from "./component/Loading";
+import { Layout, Button, BackTop } from "antd";
+import firebase from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { setUser, clearUser } from "./redux/actions/user_action";
+import * as antIcon from "react-icons/ai";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+
+const { Sider, Content, Header } = Layout;
+function App(props) {
+
+  let history = useHistory();
+  let dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.user.isLoading);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        history.push("/");
+        dispatch(setUser(user));
+      } else {
+        history.push("/login");
+        dispatch(clearUser());
+      }
+    });
+  }, []);
+  
+
+  // 스크롤 이벤트 핸들러
+  const [TopFix, setTopFix] = useState(false);
+  const [TopFixLeft, setTopFixLeft] = useState(false);
+  const handleScroll = () => {
+    let scrollTop = document.documentElement.scrollTop;
+    if (scrollTop >= 100) {
+      setTopFixLeft(true);
+    } else {
+      setTopFixLeft(false);
+    }
+    if (scrollTop >= 70) {
+      setTopFix(true);
+    } else {
+      setTopFix(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+  if (isLoading) {
+    return (
+      <>
+        <Layout className={TopFix && "top-fix"}>
+          <Header className="header-box">
+            <Link to="/join">
+                join
+            </Link>
+          </Header>
+          <Layout>
+            <div className="content-box">
+              
+              <Content>
+                <Loading />
+              </Content>
+            </div>
+          </Layout>
+        </Layout>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Layout className={TopFix && "top-fix"}>
+          <Header className="header-box">
+            <Link to="/join">
+                join
+            </Link>
+          </Header>
+          <Layout>
+            <div className="content-box">
+              
+              <Content>
+                <Switch>
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/join" component={Join} />
+                </Switch>
+              </Content>
+              <BackTop>
+                <Button
+                  type="primary"
+                  shape="circle"
+                  className="btn-top-move"
+                  icon={<antIcon.AiOutlineArrowUp />}
+                />
+              </BackTop>
+            </div>
+          </Layout>
+        </Layout>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/Join.js
@@ -0,0 +1,161 @@
+import React, { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import firebase from "../firebase";
+
+function Join() {
+  const { register, handleSubmit, watch, errors } = useForm({
+    mode: "onChange",
+  });
+  const [errorFromSubmit, setErrorFromSubmit] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const password = useRef();
+  password.current = watch("password");
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      let createdUser = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(data.email, data.password);
+
+      await createdUser.user.updateProfile({
+        displayName: data.name,
+        photoURL: data.part,
+      });
+      //Firebase 데이터베이스에 저장해주기
+      await firebase.database().ref("users").child(createdUser.user.uid).set({
+        name: createdUser.user.displayName,
+        email: data.email,
+        role: 0,
+      });
+      setLoading(false);
+    } catch (error) {
+      setErrorFromSubmit(error.message);
+      setTimeout(() => {
+        setErrorFromSubmit("");
+        setLoading(false);
+      }, 3000);
+    }
+  };
+
+  watch("name");
+  watch("email");
+  watch("password");
+  watch("password2");
+
+  const [InputName, setInputName] = useState(false);
+  const [InputEmail, setInputEmail] = useState(false);
+  const [InputPw, setInputPw] = useState(false);
+  const [InputPw2, setInputPw2] = useState(false);
+
+  const onInputName = (e) => {
+    setInputName(e.target.value);
+  };
+  const onInputEmail = (e) => {
+    setInputEmail(e.target.value);
+  };
+  const onInputPw = (e) => {
+    setInputPw(e.target.value);
+  };
+  const onInputPw2 = (e) => {
+    setInputPw2(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="join-form-wrap">
+        <form className="join-form" onSubmit={handleSubmit(onSubmit)}>
+          <div className="input-box">
+            <input
+              type="text"
+              onChange={onInputName}
+              name="name"
+              id="name"
+              ref={register({ required: true })}
+            />
+            <label
+              htmlFor="name"
+              className={"place-holder " + (InputName && "on")}
+            >
+              <span>이름</span>
+            </label>
+            {errors.name && errors.name.type === "required" && (
+              <p>이름을 입력해 주세요</p>
+            )}
+          </div>
+          <div className="input-box">
+            <input
+              name="email"
+              type="email"
+              id="email"
+              onChange={onInputEmail}
+              ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+            />
+            <label
+              htmlFor="email"
+              className={"place-holder " + (InputEmail && "on")}
+            >
+              <span>이메일</span>
+            </label>
+            <span style={{color:"#888",fontSize:"12px",display:"block",paddingLeft:"10px",marginTop:"3px"}}>※ 실제 사용중인 이메일로 가입 바랍니다(비밀번호 재설정 시 필요)</span>
+            {errors.email && errors.email.type === "required" && (
+              <p>이메일을 입력해 주세요</p>
+            )}
+            {errors.email && errors.email.type === "pattern" && (
+              <p>이메일 형식이 맞지 않습니다.</p>
+            )}
+          </div>
+          <div className="input-box">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={onInputPw}
+              ref={register({ required: true, minLength: 6 })}
+            />
+            <label
+              htmlFor="password"
+              className={"place-holder " + (InputPw && "on")}
+            >
+              <span>비밀번호</span>
+            </label>
+            {errors.password && errors.password.type === "required" && (
+              <p>비밀번호를 입력해 주세요</p>
+            )}
+            {errors.password && errors.password.type === "minLength" && (
+              <p>비밀번호는 최소 6글자이상 이어야 합니다.</p>
+            )}
+          </div>
+          <div className="input-box">
+            <input
+              type="password"
+              name="password2"
+              id="password2"
+              onChange={onInputPw2}
+              ref={register({
+                required: true,
+                validate: (value) => value === password.current,
+              })}
+            />
+            <label
+              htmlFor="password2"
+              className={"place-holder " + (InputPw2 && "on")}
+            >
+              <span>비밀번호 확인</span>
+            </label>
+            {errors.password2 && errors.password2.type === "required" && (
+              <p>비밀번호 확인을 입력해 주세요</p>
+            )}
+            {errors.password2 && errors.password2.type === "validate" && (
+              <p>비밀번호가 일치하지 않습니다.</p>
+            )}
+            {errorFromSubmit && <p>{errorFromSubmit}</p>}
+          </div>
+          <input type="submit" value="회원가입" disabled={loading} />
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Join;
